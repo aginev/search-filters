f50e95aff213dd6df4c5e75478943ba4bb44b368
Atanas Ginev <atanas.ginev@gmail.com>
2016-02-07 15:00:37 +0200

    Filterable trait added

---
+++ src/Filterable.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Aginev\SearchFilters;
+
+trait Filterable
+{
+    /**
+     * @var Filter
+     */
+    public $filter;
+
+    /**
+     * Filter scope
+     *
+     * @param $query
+     * @param $request
+     * @return mixed
+     */
+    public function scopeFilter($query, $request)
+    {
+        // Create ne filter object
+        $this->filter = new Filter($query, $request);
+
+        // Set the filters if any defined
+        $this->setFilters();
+
+        // Call order always after the all filters
+        $this->filter->order();
+
+        // Object are always passed by reference
+        // http://php.net/manual/en/language.oop5.references.php
+        return $query;
+    }
+
+    /**
+     * Set query filters
+     *
+     * Overwrite this method in the model to set query filters
+     */
+    public function setFilters()
+    {
+        //
+    }
+}
