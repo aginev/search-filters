ee0909d92a4c19fb5a9df3c35adf34a73f5049c2
Atanas Ginev <atanas.ginev@gmail.com>
2016-02-07 15:00:15 +0200

    Service provider added

---
+++ src/SearchFiltersServiceProvider.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Aginev\SearchFilters;
+
+use Illuminate\Support\ServiceProvider;
+
+class SearchFiltersServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot()
+    {
+        // Merge config
+        $this->mergeConfigFrom(base_path('vendor/aginev/search-filters/config/search-filters.php'), 'search-filters');
+        // Publish config
+        $this->publishes([
+            base_path('vendor/aginev/search-filters/config/search-filters.php') => config_path('search-filters.php'),
+        ], 'config');
+    }
+
+}
