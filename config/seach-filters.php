286ff5db0628c5a9c0f3fdf94caf63bb7ea87cda
Atanas Ginev <atanas.ginev@gmail.com>
2016-02-07 14:59:51 +0200

    Config file added

---
+++ config/seach-filters.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    /**
+     * Default order by column
+     */
+    'order_by'      => 'id',
+
+    /**
+     * Default order by key
+     */
+    'order_by_key'  => 'order_by',
+
+    /**
+     * Default order direction
+     */
+    'order_dir'     => 'desc',
+
+    /**
+     * Default order direction key
+     */
+    'order_dir_key' => 'order_dir',
+];
